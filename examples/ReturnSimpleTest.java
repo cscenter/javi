5edb814a8a23d03858d4fee45eddf68213f5aa11
Sheina Ekaterina <sheina.ekaterina.s@yandex.ru>
2015-04-11 13:00:22 +0300

    Fixed return test.

--- examples/ReturnSimpleTest.java
+++ examples/ReturnSimpleTest.java
@@ -4,11 +4,9 @@ public class ReturnSimpleTest {
     public static String main(String[] args) {
         int i = 0;
         if (i != 0) {
-            g = "sdfsd";
             return "Hello Bad";
         }
         int a = 0;
-        a = 5;
         return "Hello";
     }
 }
